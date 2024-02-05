63990156bf484de5db6b03406f6fab1178410818
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2024-02-05 11:16:16 -0600

    chore: bump

--- test/test-module/singleFile.js
+++ test/test-module/singleFile.js
@@ -1,5 +1,7 @@
 export function foo(bar) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/restrict-plus-operands
   return bar + 1;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 global.foo = foo(2);
